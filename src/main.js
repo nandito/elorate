8532a951e6e53b2849f833f7b62c6db6dc5baaeb
Nandor Biro <nandor.biro@digitalnatives.hu>
2016-11-28 18:30:31 +0100

    Initial commit - basic React setup

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+    <div>
+      Hello Rating
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
