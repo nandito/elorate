38c2d2b5f147aed082989b46ff323c4b7f9ad186
Nandor Biro <nandor.biro@digitalnatives.hu>
2016-11-28 19:06:47 +0100

    Add redux

--- src/main.js
+++ src/main.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
 
-const App = () => {
-  return (
-    <div>
-      Hello Rating
-    </div>
-  )
-}
+import App from './components/App'
+import * as reducers from './reducers.js'
+
+const store = createStore( combineReducers(reducers) )
+
+let state = store.getState()
+console.log(state)
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 )
+
+// TODO:
+// 1. Collect items
+// 2. Fight items
+// 3. Show scores
