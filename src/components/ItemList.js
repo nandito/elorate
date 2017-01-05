b789b2c6c7b9094d49a1a6d1a5a47465685d4331
Nandor Biro <nandor.biro@digitalnatives.hu>
2017-01-05 22:32:43 +0100

    Add remove item function

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,40 +1,55 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { removeItem } from '../actions.js'
 
 const mapStateToProps = ({ items }) => ({
   items
 })
 
-const ItemList = ({ items }) => {
-  return (
-    <div className="item-list">
-      <p style={{
-        display: (items.length > 0 ? 'none' : 'block')
-      }} >
-        There are no items added.
-      </p>
+const mapDispatchToProps = dispatch => ({
+  removeItem: id => dispatch(removeItem(id))
+})
 
-      <table
-        style={{
-          display: (items.length > 0 ? 'block' : 'none')
+class ItemList extends React.Component {
+  render() {
+    const { items } = this.props
+
+    const onDelete = (id) => {
+      this.props.removeItem(id)
+    }
+
+    return (
+      <div className="item-list">
+        <p style={{
+          display: (items.length > 0 ? 'none' : 'block')
         }} >
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Score</th>
-          </tr>
-        </thead>
-        <tbody>
-          {items.map((item) =>
-            <tr key={item.id}>
-              <td>{item.name}</td>
-              <td>{item.score}</td>
+          There are no items added.
+        </p>
+
+        <table
+          style={{
+            display: (items.length > 0 ? 'block' : 'none')
+          }} >
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Score</th>
+              <th></th>
             </tr>
-          )}
-        </tbody>
-      </table>
-    </div>
-  )
+          </thead>
+          <tbody>
+            {items.map((item) =>
+              <tr key={item.id}>
+                <td>{item.name}</td>
+                <td>{item.score}</td>
+                <td><button onClick={onDelete.bind(null, item.id)}>Remove</button></td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
 
-export default connect(mapStateToProps)(ItemList)
+export default connect(mapStateToProps, mapDispatchToProps)(ItemList)
