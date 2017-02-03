9aaadb9838e377317a5b41a627e52603ceea7d3a
Nandor Biro <nandor.biro@digitalnatives.hu>
2017-02-03 17:22:19 +0100

    Navigation styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router'
 
-// TODO: navigation facelift:
-// * inline list items
-// * visual separation
-
 const Navigation = () => (
-  <div className="row">
-    Navigation
-    <ul>
-      <li><Link to='/'>Home</Link></li>
-      <li><Link to='/collect'>Collect</Link></li>
-      <li><Link to='/rate'>Rate</Link></li>
-    </ul>
+  <div className="row-flex flex-justify-content-start">
+    <Link className="button-stripped" to='/'>Home</Link>
+    <Link className="button-stripped" to='/collect'>Collect</Link>
+    <Link className="button-stripped" to='/rate'>Rate</Link>
   </div>
 )
 
